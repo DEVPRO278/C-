ed62e9d7d70b95e091de91ff28afa5de0e2e21a6
DEVPRO278 <soltan.nb93@gmail.com>
2017-02-01 19:44:02 +0000

    First C++ Add

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+    
+    string x ;
+    cout << " Hello SOUHAIL \n";
+    
+    cout<< "Labas 3lik ? \n" ;
+    
+    cout<<"endl";
+    
+    cout << " OUI / NON  ?? \n" ;
+    
+    cin >> x ;
+    
+    for ( int i=1 ;i<=5 ;i++ )
+    {
+    cout << " vous choizer : " + x + "\n" ;
+    }
+    
+    
+    
+    
+    
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
